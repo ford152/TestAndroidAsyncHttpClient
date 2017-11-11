2e2895a862d06dafc37491e063d9ce04bf1687ef
baykalov-maxim <baykalov.maxim@gmail.com>
2017-11-12 01:25:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oem/test/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.oem.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.Header;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tW;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tW = (TextView)findViewById(R.id.textView);
+    }
+
+    public void sendHttp(View view) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                System.out.println(new String(response));
+                tW.setText(new String(response));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
+    }
+}
