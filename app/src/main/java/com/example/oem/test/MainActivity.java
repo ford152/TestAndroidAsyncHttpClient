b6df93ec883435ffee99b472f86915c58fd2456c
baykalov-maxim <baykalov.maxim@gmail.com>
2017-12-04 21:40:23 +0200

    Change ACH to OkHttp. Add Support sdk 21

--- app/src/main/java/com/example/oem/test/MainActivity.java
+++ app/src/main/java/com/example/oem/test/MainActivity.java
@@ -1,17 +1,45 @@
 package com.example.oem.test;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.loopj.android.http.*;
-import cz.msebera.android.httpclient.Header;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tW;
+
+    public MainActivity() throws NoSuchAlgorithmException {
+
+    }
+
+
+    String run() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .header("Host", "devhk3.azureedge.net")
+                .sni("devhk3.azureedge.net")
+                .url("https://software-download.office.microsoft.com")
+                .build();
+
+        ResponseBody body = client.newCall(request).execute().body();
+        if (body == null) {
+            return "body is empty";
+        }
+        return body.string();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +50,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendHttp(View view) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("q", "github");
-        client.addHeader("accept-language", "de");
+        new MyTask().execute();
+    }
 
-        client.get("https://www.google.com.ua/search", params, new AsyncHttpResponseHandler() {
 
-            @Override
-            public void onStart() {
-                // called before request is started
+    class MyTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return run();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            return null;
+        }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                System.out.println(new String(response));
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
-            }
-        });
+        @Override
+        protected void onPostExecute(String result) {
+            tW.setText(result);
+            super.onPostExecute(result);
+        }
     }
 }
